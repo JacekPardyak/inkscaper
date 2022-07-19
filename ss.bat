e05e97635b7f25b5f89ad3f3475c0f4ce99a214c
unknown <jacek.pardyak@gmail.com>
2022-07-20 00:15:53 +0200

    go

---
+++ ss.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cd C:\Program Files\Inkscape\bin\
+python.exe "C:\Program Files\Inkscape\share\inkscape\extensions\dxf12_outlines.py" --output="C:\Users\jacek\inkscaper\Red_Bird.dxf"  "C:\Users\jacek\inkscaper\Red_Bird.svg"
