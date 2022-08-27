3bcde85c957241ab88f189cc44c0be9966e78e5d
unknown <jacek.pardyak@gmail.com>
2022-08-27 23:22:57 +0200

    cc

---
+++ R/inx_sf2svg.R
@@ -0,0 +1,23 @@
+#' Title
+#'
+#' @param geom simple features
+#'
+#' @return file name
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' library(sf)
+#' filename <- system.file("shape/nc.shp", package="sf")
+#' nc <- st_read(filename)
+#' inx_sf2svg(nc)
+inx_sf2svg <- function(geom){
+  inx_dxf <- tempfile("inx", fileext = c(".dxf"))
+  geom %>%
+  sf::st_geometry() %>%
+  sf::st_write(dsn = inx_dxf, driver ="DXF", delete_dsn = TRUE)
+  inx_svg <- inx_dxf %>% inx_extension(inkscape_extension_name = "dxf_input.py", ext = ".svg") %>%
+  inx_actions(actions = 'export-area-drawing', ext = ".svg")
+}
+
+
