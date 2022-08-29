c90f6733cdfadb241e856d7fc008089db6d1da0d
Jacek Pardyak <jacek.pardyak@nl.team.blue>
2022-08-29 13:27:12 +0200

    cmt

--- R/inx_svg2sf.R
+++ R/inx_svg2sf.R
@@ -2,7 +2,7 @@
 #'
 #' @param input file or url of the SVG
 #'
-#' @return file name
+#' @return simple features
 #' @export
 #'
 #' @examples
@@ -10,16 +10,15 @@
 #' library(sf)
 #' input = system.file("extdata", "R_logo.svg", package = "inkscaper", mustWork = TRUE)
 #' input %>%
-#' inx_svg2dxf() %>%
-#' st_read() %>%
+#' inx_svg2sf() %>%
 #' ggplot() +
 #'   geom_sf()
 #'
 #' "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/tiger.svg" %>%
-#' inx_svg2dxf() %>%
-#' st_read() %>%
+#' inx_svg2sf() %>%
 #' ggplot() +
 #'   geom_sf()
-inx_svg2dxf <- function(input){
-  inx_extension(input, inkscape_extension_name = "dxf12_outlines.py", ext = ".dxf")
+inx_svg2sf <- function(input){
+  inx_extension(input, inkscape_extension_name = "dxf12_outlines.py", ext = ".dxf") %>%
+  sf::st_read()
 }
